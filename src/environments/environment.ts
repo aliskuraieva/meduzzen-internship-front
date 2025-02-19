f30e9d357fa04cb423441d636ee0ddb4a086c91f
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-02-19 15:21:05 +0200

    edit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: process.env['NG_APP_API_URL'],
-  dbConnection: process.env['DB_CONNECTION'],
   auth0: {
     domain: process.env['AUTH0_DOMAIN'],
     clientId: process.env['AUTH0_CLIENT_ID'],
