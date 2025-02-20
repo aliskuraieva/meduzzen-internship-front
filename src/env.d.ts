6e5b7b7258202df21c8b226abc50389da28cd935
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-02-20 13:39:59 +0200

    .

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,7 @@
 declare interface Env {
   readonly NODE_ENV: string;
-  readonly NG_APP_API_URL: string;
-  readonly NG_APP_AUTH0_DOMAIN: string;
-  readonly NG_APP_AUTH0_CLIENT_ID: string;
-  readonly NG_APP_AUTH0_AUDIENCE: string;
+
+  [key: string]: any;
 }
 
 declare interface ImportMeta {
