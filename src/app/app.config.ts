1dcb0fa4ce4e4c715205f689933b1bb63273f6f9
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-02-20 13:01:57 +0200

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAuth0 } from '@auth0/auth0-angular';
 import { provideToastr } from 'ngx-toastr';
+import { provideStore } from '@ngrx/store';
 import { appRoutes } from '../app/core/routes/app.routes';
+import { appReducer } from '../app/core/state/app.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
+    provideStore({ testString: appReducer }),
     provideHttpClient(),
     provideAuth0({
       domain: import.meta.env['NG_APP_PUBLIC_AUTH0_DOMAIN'],
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAuth0 } from '@auth0/auth0-angular';
-import { provideStore } from '@ngrx/store';
 import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/core/routes/app.routes';
-import { appReducer } from './app/core/state/app.reducer';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes),
-    provideStore({ testString: appReducer }),
-    provideHttpClient(),
-    provideAuth0({
-      domain: import.meta.env['NG_APP_PUBLIC_AUTH0_DOMAIN'],
-      clientId: import.meta.env['NG_APP_PUBLIC_AUTH0_CLIENT_ID'],
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-        audience: import.meta.env['NG_APP_PUBLIC_AUTH0_AUDIENCE'],
-        scope: 'openid profile email',
-      },
-    }),
-  ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
