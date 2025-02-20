398b3fa2375f48da1e85d4859facc6870b050268
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-02-20 14:33:16 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+if (window.__env__ && window.__env__['NG_APP_PUBLIC_API_URL']) {
+  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+} else {
+  console.error('Environment variables are not defined or loaded properly');
+}
