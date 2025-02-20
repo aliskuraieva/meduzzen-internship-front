88682bbd7d3bd88a43750bb2542f3642adabcb30
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-02-20 13:37:53 +0200

    added syntax

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,9 @@
 declare interface Env {
   readonly NODE_ENV: string;
-
-  [key: string]: any;
+  readonly NG_APP_API_URL: string;
+  readonly NG_APP_AUTH0_DOMAIN: string;
+  readonly NG_APP_AUTH0_CLIENT_ID: string;
+  readonly NG_APP_AUTH0_AUDIENCE: string;
 }
 
 declare interface ImportMeta {
